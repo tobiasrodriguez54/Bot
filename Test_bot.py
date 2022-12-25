aae9b8279c98263ddfd70ae4322d2bbbf387d509
tobiasrodriguez54 <74516165+tobiasrodriguez54@users.noreply.github.com>
2022-12-25 16:04:36 -0300

    Update Test_bot.py

--- Test_bot.py
+++ Test_bot.py
@@ -31,11 +31,11 @@ if __name__ == "__main__":
     hilo_bot.start()
     print('Bot Iniciado')
 
-while seconds != 86400:
+while seconds != 57605:
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     date_time = dt.datetime.strptime(current_time, "%H:%M:%S")
     a_timedelta = date_time - dt.datetime(1900, 1, 1)
     seconds = a_timedelta.total_seconds()
-    if seconds == 86400:
+    if seconds == 57605:
         imprimir_mensaje()
